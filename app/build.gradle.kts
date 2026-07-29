974bfabc356cbb8069e6da23fb9d17c40a8fbad3
Elnur Ibrahimov <elnuribrahimov83@gmail.com>
2026-07-30 03:25:26 +0400

    release: v0.39.1 (round8 deep review fixes)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.aura"
         minSdk = 26
         targetSdk = 35
-        versionCode = 47
-        versionName = "0.39.0"
+        versionCode = 48
+        versionName = "0.39.1"
         testInstrumentationRunner = "com.aura.testing.HiltTestRunner"
         vectorDrawables { useSupportLibrary = true }
     }
