1bf58539feee407d41cc5bdbade0c3667f038f68
Elnur Ibrahimov <elnuribrahimov83@gmail.com>
2026-08-03 19:08:02 +0400

    release: v0.57.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.aura"
         minSdk = 26
         targetSdk = 35
-        versionCode = 69
-        versionName = "0.57.0"
+        versionCode = 70
+        versionName = "0.57.1"
         testInstrumentationRunner = "com.aura.testing.HiltTestRunner"
         vectorDrawables { useSupportLibrary = true }
     }
